397d6bad0fd56cb2bdb8dd5bf72f10dee1fc7f07
RenoIM <168320885+RenoIM@users.noreply.github.com>
2024-05-03 19:44:57 +0700

    Add files via upload

---
+++ Tugas 2 Part 2 main.py
@@ -0,0 +1,15 @@
+n = -1
+while (n < 1) or (n > 20):
+    n = int(input("Masukan Nilai Non negatif (1 - 20): "))
+    if (n < 1) or (n > 20):
+        print("Nilai harus sekitaran dari 1 - 20")
+
+n = n + 1
+i = 0
+t = 0
+print()
+for i in range (0, n, 1):
+    t = i * i
+    if t % 2 or i == 0 : 
+        print(t)
+    i = i + 1
